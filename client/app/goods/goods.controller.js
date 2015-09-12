3567ae16576173111145ba162a871e472eb85352
Julia <julia.nesteruk@gmail.com>
2015-09-12 15:28:32 +0300

    Style changes to main page and navigation

--- client/app/goods/goods.controller.js
+++ client/app/goods/goods.controller.js
@@ -75,6 +75,7 @@ angular.module('cheapTodayApp')
 
     $scope.setCurrentPage = function (p) {
       $scope.currentPage = p;
+      window.scrollTo(0,0);
     };
 
     $scope.resetPagination = function () {
