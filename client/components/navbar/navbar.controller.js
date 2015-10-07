3492fae7a694d32f613140e25269a52ed5c90cda
Julia <julia.nesteruk@gmail.com>
2015-10-07 12:27:42 +0300

    reverting the revert t Please enter the commit message for your changes. Lines starting

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,9 +2,15 @@
 
 angular.module('cheapTodayApp')
   .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [/*{ 'title': 'Товари', 'link': '/goods' }*/];
+
+    $scope.menu = [
+      /*{'title': 'Знижки та розпродажі', 'link': '/promo'},
+      {'title': 'Акційні товари', 'link': '/goods'},
+      {'title': 'Контакти', 'link': '/contact'}*/
+    ];
 
     $scope.isActive = function (route) {
       return route === $location.path();
     };
+
   });
