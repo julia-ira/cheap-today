38902c7e7b2cb7400f2cf5cc33c5c99440237e9a
Julia <julia.nesteruk@gmail.com>
2015-10-09 13:23:12 +0300

    Filter improvements

--- client/app/goods/goods.controller.js
+++ client/app/goods/goods.controller.js
@@ -4,25 +4,45 @@ angular.module('cheapTodayApp')
   .controller('GoodsCtrl', function ($scope, $filter, $http, goods) {
 
     $scope.filteredGoods = [];
-    $scope.allGoods = [];
     $scope.categories = [];
+    $scope.numOfGoods = 0;
     $scope.currentPage = 0;
     $scope.itemsPerPage = 20;
-    $scope.stores = [{name:"Comfy",id:"comfy"},{name:"Ельдорадо",id:"eldorado"},{name:"КТС",id:"ktc"},{name:"Фокстрот",id:"foxtrot"},{name:"Алло",id:"allo"},{name:"Метро",id:"metro"}];
+    $scope.stores = [
+      {name: "Comfy", id: "comfy"},
+      {name: "Ельдорадо", id: "eldorado"},
+      {name: "КТС", id: "ktc"},
+      {name: "Фокстрот", id: "foxtrot"},
+      {name: "Алло", id: "allo"},
+      {name: "Метро", id: "metro"}
+    ];
 
-    $(function() {
-       $(".filtering-nav li").click(function() {
-          $(".filtering-nav li").not(this).removeClass("active");
-          $(this).toggleClass("active");
-          var navitem = "#filtering-expand-" + $(this).attr("data-expand");
-          $(".filtering-expand").not(navitem).slideUp("normal");
-          $(navitem).slideToggle("normal");
-       });
-       $("#filterPrice").ionRangeSlider({
-          onChange: function (data){
-            $scope.filterGoodsByPrice(data.from,data.to);
+    $(function () {
+      $(".filtering-nav li").click(function () {
+        $(".filtering-nav li").not(this).removeClass("active");
+        $(this).toggleClass("active");
+        var navitem = "#filtering-expand-" + $(this).attr("data-expand");
+        $(".filtering-expand").not(navitem).slideUp("normal");
+        $(navitem).slideToggle("normal");
+      });
+      $("#filterPrice").ionRangeSlider({
+        onFinish: function (data) {
+          if (!_.isUndefined($scope.filterStrategy)) { // should be a better solution to assign data.from and data.to
+            $scope.filterStrategy.price = {
+              min: data.from,
+              max: data.to
+            };
+          } else {
+            $scope.filterStrategy = {
+              price: {
+                min: data.from,
+                max: data.to
+              }
+            };
           }
-       });
+          $scope.filterGoods($scope.filterStrategy);
+        }
+      });
     });
 
     function formArrayFrom1toN(N) {
@@ -58,12 +78,17 @@ angular.module('cheapTodayApp')
 
     function init(val) {
       $scope.filteredGoods = val;
-      $scope.allGoods = val;
+      $scope.numOfGoods = val.length;
       $scope.setPages();
       $scope.categories();
     }
 
-    $scope.categories = function() {
+    $scope.cleanFilterStrategy = function () { // should also be cleaned ionRangeSlider!!
+      $scope.filterStrategy = {};
+      $scope.filteredGoods = loadFromLocalStorage();
+    };
+
+    $scope.categories = function () {
       $http.get('/resources/categories.json').then(function (categories) {
         $scope.categories = categories.data;
       });
@@ -75,7 +100,7 @@ angular.module('cheapTodayApp')
 
     $scope.setCurrentPage = function (p) {
       $scope.currentPage = p;
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
     };
 
     $scope.resetPagination = function () {
@@ -83,31 +108,35 @@ angular.module('cheapTodayApp')
       $scope.setPages();
     };
 
-    $scope.findGoods = function (txt) {
-      $scope.filteredGoods = $filter('filter')($scope.allGoods, txt, false);
+    $scope.findGoods = function (txt) { // doesn't work
+      $scope.cleanFilterStrategy();
+      $filter('filter')($scope.filteredGoods, txt, false);
       $scope.resetPagination();
     };
 
-    $scope.filterGoodsByPrice = function (min, max) {
-      console.log(min);
-      console.log(max);
-      $scope.filteredGoods = $filter('goodsByPrice')($scope.allGoods, {min: min, max: max}, false);
-      $scope.resetPagination();
-    };
+    $scope.filterGoods = function () {
+      console.log($scope.filterStrategy);
+      $scope.filteredGoods = loadFromLocalStorage();
 
-    $scope.filterGoodsByStore = function (store) {
-      console.log($scope.filteredGoods);
-      console.log($scope.allGoods);
-      $scope.filteredGoods = $filter('goodsByShop')($scope.allGoods, store, false);
-      $scope.resetPagination();
-    };
+      if (!_.isUndefined($scope.filterStrategy.price)) { // UI is not refreshed after this filter
+        $filter('goodsByPrice')($scope.filteredGoods, {
+          min: $scope.filterStrategy.price.min,
+          max: $scope.filterStrategy.price.max
+        }, false);
+      }
 
-    $scope.filterGoodsByCategory = function (categoryId) {
-        $scope.filteredGoods = $filter('goodsByCategory')($scope.allGoods, {
-          categoryId: categoryId,
+      if (!_.isUndefined($scope.filterStrategy.store)) {
+        $filter('goodsByShop')($scope.filteredGoods, $scope.filterStrategy.store.id, false);
+      }
+
+      if (!_.isUndefined($scope.filterStrategy.category)) {
+        $filter('goodsByCategory')($scope.filteredGoods, {
+          categoryId: $scope.filterStrategy.category.id,
           categories: $scope.categories
         }, false);
-        $scope.resetPagination();
+      }
+
+      $scope.resetPagination();
     };
 
     loadGoods();
